d0d8b075a698b10a9aa587cb490e5403150543cc
sik1j <karkisikij2@gmail.com>
2024-06-07 12:17:33 -0700

    Encapsulate lexer functions into lexer impl

--- src/lexer.rs
+++ src/lexer.rs
@@ -2,7 +2,7 @@ use std::io::Read;
 use std::str::FromStr;
 use crate::lexer::Token::Char;
 
-#[derive(Debug)]
+#[derive(Debug, Clone, PartialEq)]
 pub enum Token {
     Eof,
 
@@ -18,47 +18,61 @@ pub enum Token {
     Char(char)
 }
 
-fn get_char() -> char {
-    let mut buffer = [0;1];
-    let stdin = std::io::stdin();
-    let mut handle = stdin.lock();
-
-    handle.read_exact(&mut buffer).map_or(0 as char, |_| {buffer[0] as char})
+pub struct Lexer {
+    pub cur_tok: Token
 }
 
-/**
-    gets the next token from stdin
- */
-pub fn get_tok() -> Option<Token> {
-    let mut last_char = ' ';
-    while last_char.is_whitespace() {last_char = get_char(); }
+impl Lexer {
+    fn get_char() -> char {
+        let mut buffer = [0;1];
+        let stdin = std::io::stdin();
+        let mut handle = stdin.lock();
 
-    match last_char {
-        'a'..='z' | 'A'..='Z' => Some({
-            let mut identifier = String::from(last_char);
-            while { last_char = get_char(); last_char.is_alphanumeric() } {
-                identifier.push(last_char);
-            };
-            match identifier.as_str() {
-                "def" => Token::Def,
-                "extern" => Token::Extern,
-                _ => Token::Identifier(identifier),
+        handle.read_exact(&mut buffer).map_or(0 as char, |_| {buffer[0] as char})
+    }
+
+    /**
+    gets the next token from stdin
+     */
+    pub fn get_tok() -> Option<Token> {
+        let mut last_char = ' ';
+        while last_char.is_whitespace() {last_char = Lexer::get_char(); }
+
+        match last_char {
+            'a'..='z' | 'A'..='Z' => Some({
+                let mut identifier = String::from(last_char);
+                while { last_char = Lexer::get_char(); last_char.is_alphanumeric() } {
+                    identifier.push(last_char);
+                };
+                println!("matching iden: {}", last_char);
+                match identifier.as_str() {
+                    "def" => Token::Def,
+                    "extern" => Token::Extern,
+                    _ => Token::Identifier(identifier.clone()),
+                }
+            }),
+            '0'..='9' | '.' => {
+                let mut num_str = String::from(last_char);
+                while {last_char = Lexer::get_char(); last_char.is_alphanumeric() || last_char == '.'} {
+                    num_str.push(last_char);
+                }
+                f64::from_str(num_str.as_str()).ok().map(|n| {Token::Number(n)})
+            },
+            '#' => {
+                while {last_char = Lexer::get_char(); last_char != '\0' && last_char != '\n' && last_char != '\r'} {};
+                Lexer::get_tok()
             }
-        }),
-        '0'..='9' | '.' => {
-            let mut num_str = String::from(last_char);
-            while {last_char = get_char(); last_char.is_alphanumeric() || last_char == '.'} {
-                num_str.push(last_char);
-            }
-            f64::from_str(num_str.as_str()).ok().map(|n| {Token::Number(n)})
-        },
-        '#' => {
-            while {last_char = get_char(); last_char != '\0' && last_char != '\n' && last_char != '\r'} {};
-            get_tok()
+            '\0' => {Some(Token::Eof)},
+            _ => {
+                Some(Char(last_char))
+            },
         }
-        '\0' => {Some(Token::Eof)},
-        _ => {
-            Some(Char(last_char))
-        },
+    }
+
+    pub fn get_next_tok(&mut self) {
+        match Self::get_tok() {
+            None => {panic!("Could not get token")}
+            Some(tok) => {self.cur_tok = tok;}
+        }
     }
 }
